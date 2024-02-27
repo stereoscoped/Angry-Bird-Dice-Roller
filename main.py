4adafa9b3f22ff0694ee4056797044e7b1d139af
Matthew So <96310058+stereoscoped@users.noreply.github.com>
2024-02-26 23:45:50 -0800

    terminal version

--- main.py
+++ main.py
@@ -5,8 +5,42 @@ by stereoscoped
 """
 
 import random
+from PIL import ImageTk,Image
+import time
+import tkinter
 
-while True:
-    bird = random.random()
+
+# Creates the main window object
+window = tkinter.Tk()
+window.grid()
+window.geometry("400x400")
+window.maxsize(400, 400)
+window.minsize(400,400)
+window.title("Angry Bird Dice Roller")
+
+def button_press():
+    print("test")
+    bird = random.randint(0, 5)
     print(bird)
 
+
+# Canvas for bird image
+canvas = tkinter.Canvas(window)
+red = ImageTk.PhotoImage(Image.open("images/red.png"))
+canvas.pack()
+canvas.create_image(180,125, image=red)
+
+def show_red():
+    canvas = tkinter.Canvas(window)
+    red = ImageTk.PhotoImage(Image.open("images/blue.png"))
+    canvas.itemconfig(image_container,image=img2)
+    canvas.pack()
+    canvas.create_image(180,125, image=red)
+
+# Creates the roll button
+roll_button = tkinter.Button(window, text="Roll", width=25, command=lambda:show_red)
+roll_button.pack()
+
+
+# Infinite loop for window
+window.mainloop()
---
+++ terminal_version.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+while True:
+
+    roll = input("Enter anything to roll! ")
+
+    roll = random.randint(0,5)
+    if roll == 0:
+        print("Red")
+    elif roll == 1:
+        print("Blues")
+    elif roll == 2:
+        print("Bomb")
+    elif roll == 3:
+        print("Chuck")
+    elif roll == 4:
+        print("Matilda")
+    elif roll == 5:
+        print("Terence")
+    
+    time.sleep(1)
